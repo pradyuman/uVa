4aa84ed4bac270084f2c2d43c9564da962a730ee
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-16 17:58:46 -0500

    Fixed problem 10055 for ANSI compiler

--- 10055 - Hashmat the Brave Warrior/10055.c
+++ 10055 - Hashmat the Brave Warrior/10055.c
@@ -1,14 +1,13 @@
-//Solution to 1055 problem
+/*Solution to 1055 problem*/
 #include <stdio.h>
 #include <string.h>
 
 int main(){
-	//Variables
+	/*Variables*/
 	long long num1, num2;
-	while(scanf("%lld %lld", &num1, &num2)){ 
-		if(num2 > num1)
-			printf("%lld\n", num2 - num1);
-		else
-			printf("%lld\n", num1 - num2);
+	while(scanf("%lld %lld", &num1, &num2) == 2){ 
+		printf("%lld\n", num2>num1?num2-num1:num1-num2);	
 	}
+
+	return 0;
 }
