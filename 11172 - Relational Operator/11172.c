6da4ce6ec9ce9ba2a2062008bc05fdb9e760a25d
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-18 17:09:30 -0500

    Finished explanation for problem 11172

--- 11172 - Relational Operator/11172.c
+++ 11172 - Relational Operator/11172.c
@@ -1,20 +1,14 @@
 /*Problem 11172 */
 #import <stdio.h>
 
-int main() {
-   int num;
-   scanf("%d", &num);
-	int a, b, i;
-	if(num < 15) {
-      for(i = 0; i < num; i++){
-         scanf("%d %d", &a, &b);
-         if (a < 1000000001 && b < 1000000001 && a > -1000000001 && b > -1000000001) {
-            if(a == b)
-               printf("=\n");
-            else
-               printf("%c\n",a<b?'<':'>');
-         }
-      }
-	}
+int main(){
+   unsigned num;
+   unsigned a, b;
+   
+   scanf("%u", &num);
+   while (num--){
+      scanf("%u%u", &a, &b);
+      printf("%c\n", a > b ? '>' : a < b ? '<' : '=');
+   }
 	return 0;
 }
---
+++ 11172 - Relational Operator/README.md
@@ -0,0 +1,3 @@
+Status: **Accepted | 0.015s**
+
+This program takes the first integer and uses it as the loop control variable. It then tests all the following sets of numbers and outputs the proper relational character.
