4d451ddd9be2afcf67d77f772d95b585342963fa
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-03-24 16:55:46 -0400

    Finished problem 494

---
+++ 494 - Kndergarten Counting Game/494.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	char input[1000];
+	int i;
+	while(fgets(input, 1000, stdin)){
+		int count = 0;
+		for(i = 0; input[i] != '\0'; i++) {
+			if((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z')){
+				if((input[i+1] < 'a' || input[i+1] > 'z') && (input[i+1] < 'A' || input[i+1] > 'Z'))
+					count++;
+			}	
+		}
+		printf("%d\n", count);
+	}
+	return 0;
+}
