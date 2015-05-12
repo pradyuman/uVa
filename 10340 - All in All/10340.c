95cb34c506ca4841ffc095a66ebd8649bece8db7
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-11 21:30:36 -0500

    Finished problem 10340

--- 10340 - All in All/10340.c
+++ 10340 - All in All/10340.c
@@ -1,23 +1,23 @@
 /*Problem 10340 */
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-   int ch;
-   char ich;
-   char word[100];
-   char sub[100];
-   int i = 0; /*counter*/
-   while ((ch = getchar()) != EOF) {
-      while ((ich = getchar()) != ' ') {
-         word[i] = ch;
-         i++;
+int main() {
+   char word[1000000];
+   char sub[1000000];
+   while (scanf("%s %s", word, sub) == 2) {
+      int length = 0;
+      int j;
+      int i = 0;
+      for (j = 0; j < strlen(sub); j++) {
+         if (word[i] == sub[j]) {
+            length++;
+            i++;
+         }
       }
-      getchar();
-      i = 0;
-      while ((ch = getchar()) != '\n') {
-         sub[i] = ch;
-         i++;
-      }
+      
+      printf("%s\n", length == strlen(word) ? "Yes" : "No");
    }
-
+   
+   return 0;
 }
