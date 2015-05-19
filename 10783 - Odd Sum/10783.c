9f7a0f4a2f652809c4a71adfcd0129a2a93e49e0
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-18 21:20:05 -0500

    Finished 10783

--- 10783 - Odd Sum/10783.c
+++ 10783 - Odd Sum/10783.c
@@ -1,17 +1,44 @@
 /* Problem 10783 */
 #include <stdio.h>
 
+int findOddSum(int a, int b);
+
 int main() {
    
    int num;
    
    scanf("%d", &num);
    
+   if(num < 1 || num > 100)
+      return 1;
+   
    int a,b;
+   int count = 0;
    while (num--) {
       scanf("%d\n%d", &a, &b);
-      printf("%d %d\n", a, b);
+      
+      if (a < 0 || a > 100 || b < 0 || b > 100 || a > b)
+         return 1;
+         
+      printf("Case %d: %d\n", ++count, findOddSum(a, b));
    }
    
    return 0;
+}
+
+int findOddSum(int a, int b){
+   
+   int isOdd = a % 2;
+   
+   int lowerBound = a + !isOdd;
+   
+   int sum = 0;
+   
+   while (lowerBound <= b) {
+      sum += lowerBound;
+      lowerBound += 2;
+   }
+   
+   return sum;
+
 }
