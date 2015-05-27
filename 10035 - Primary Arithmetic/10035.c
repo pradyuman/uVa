5be7922560ab2a05e1ee5ec9e1f68895fe3bc8ec
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-27 13:23:29 -0500

    Debugging 10035

--- 10035 - Primary Arithmetic/10035.c
+++ 10035 - Primary Arithmetic/10035.c
@@ -6,41 +6,48 @@ int isCarry(int, int);
 
 int main() {
    
-   char ca[11];
-   char cb[11];
-   int a[11];
-   int b[11];
+   char ca[11], cb[11];
+   int a[10] = {0}, b[10] = {0};
    
-   int c; //carry counter
-   int i; //digit
+   int c; /*carry counter */
+   int i; /*digit*/
    
-   char magab; //magnitude
-   char carry; //is carry?
-   for (scanf("%s %s", &ca, &cb); ca != 0 && cb != 0; scanf("%s %s", &ca, &cb)) {
-      c = 0; //reset carry
+   int aLen, bLen;
+   char magab; /*magnitude*/
+   char carry; /*is carry?*/
+   
+   long int in1, in2;
+   
+   for (scanf("%ld %ld", &in1, &in2); in1 != 0 && in2 != 0; scanf("%ld %ld", &in1, &in2)) {
+      c = 0; /*reset carry*/
+
+      /*making character array of input number*/
+      aLen = sprintf(ca,"%ld", in1);
+      bLen = sprintf(cb,"%ld", in2);
       
-      aLen = strlen(ca);
-      bLen = strlen(cb);
+      for (i = 0; i < aLen; i++)
+         a[10 - aLen + i] = ca[i] - '0';
       
-      for (i = 0; i < aLen; i++) {
-         a[i] = ca[i] - '0';
+      for (i = 0; i < bLen; i++)
+         b[10 - bLen + i] = cb[i] - '0';
+
+      for (i = 9 ; i >= (aLen > bLen ? 10 - aLen : 10 - bLen); i--) {
+         carry = isCarry(a[i], b[i]);
+         c += carry; /*incrementing carry counter*/
+         if (i != 0) a[i - 1] += carry;
       }
       
-      for (i = 0; i < bLen; i++) {
-         b[i] = cb[i] - '0';
-      }
-      
-      magab = a == b ? '=' : a > b ? : 'a' : 'b';
-      
-      for (i = (a > b ? aLen : bLen) - 1; i >= 0 ; i--) {
-         carry = isCarry(atoi(a[i]), atoi(b[i]));
-         
-      }
+      if (c == 1)
+         printf("1 carry operation.\n");
+      else if (c)
+         printf("%d carry operations.\n", c);
+      else
+         printf("No carry operation.\n");
    }
 
 	return 0;
 }
 
-int isCarry(a, b) {
+int isCarry(int a, int b) {
    return (a + b) > 9 ? 1 : 0;
 }
