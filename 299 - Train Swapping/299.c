43ebb6adc35334933f0f117b06d4e1388f0aebfc
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-27 17:50:37 -0500

    Finished problem 299

--- 299 - Train Swapping/299.c
+++ 299 - Train Swapping/299.c
@@ -1,11 +1,11 @@
-/*Problem 299 */
+/*Problem 299*/
 #import <stdio.h>
 
-int swap(int carriages[]);
+int swap(int input[], int length);
 
 int main() {
    
-   int num, i, length, swap;
+   int num, i, length;
    scanf("%d", &num);
    
    int input[50];
@@ -13,12 +13,30 @@ int main() {
       scanf("%d", &length);
       for (i = 0; i < length; i++)
          scanf("%d", &input[i]);
-      printf("Optimal train swapping takes %d swaps.", swap);
+      
+      printf("Optimal train swapping takes %d swaps.\n", swap(input, length));
    }
+   
 	return 0;
 }
 
-int swap (int carraiges[]) {
-
-
+int swap(int input[], int length) {
+   int i,k;
+   int c = 0;
+   
+   int t;
+   for (i = 0; i < length; i++) {
+      k = i;
+      while (input[i] != i + 1) {
+         if (input[++k] == i + 1) {
+            t = input[k - 1];
+            input[k - 1] = input[k];
+            input[k] = t;
+            k -= 2;
+            c++;
+         }
+      }
+   }
+   
+   return c;
 }
