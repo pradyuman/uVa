a8ebcfd35cc67b8bc3d46670dab4a0b4fc7edac7
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-27 13:47:12 -0500

    Finished 10035

--- 10035 - Primary Arithmetic/10035.c
+++ 10035 - Primary Arithmetic/10035.c
@@ -2,41 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 
-int isCarry(int, int);
-
 int main() {
    
-   char ca[11], cb[11];
-   int a[10] = {0}, b[10] = {0};
+   long a, b;
    
-   int c; /*carry counter */
-   int i; /*digit*/
+   int carry, c; /*carry and carry counter*/
    
-   int aLen, bLen;
-   char magab; /*magnitude*/
-   char carry; /*is carry?*/
-   
-   long int in1, in2;
-   
-   for (scanf("%ld %ld", &in1, &in2); in1 != 0 && in2 != 0; scanf("%ld %ld", &in1, &in2)) {
-      c = 0; /*reset carry*/
-
-      /*making character array of input number*/
-      aLen = sprintf(ca,"%ld", in1);
-      bLen = sprintf(cb,"%ld", in2);
-      
-      for (i = 0; i < aLen; i++)
-         a[10 - aLen + i] = ca[i] - '0';
-      
-      for (i = 0; i < bLen; i++)
-         b[10 - bLen + i] = cb[i] - '0';
-
-      for (i = 9 ; i >= (aLen > bLen ? 10 - aLen : 10 - bLen); i--) {
-         carry = isCarry(a[i], b[i]);
-         c += carry; /*incrementing carry counter*/
-         if (i != 0) a[i - 1] += carry;
+   while (scanf("%ld %ld", &a, &b) && (a != 0 || b != 0)) {
+      carry = 0;
+      c = 0;
+      while (a != 0 || b != 0) {
+         carry = (a % 10 + b % 10 + carry) > 9 ? 1 : 0;
+         if (carry) c++;
+         a /= 10;
+         b /= 10;
       }
-      
       if (c == 1)
          printf("1 carry operation.\n");
       else if (c)
@@ -46,8 +26,4 @@ int main() {
    }
 
 	return 0;
-}
-
-int isCarry(int a, int b) {
-   return (a + b) > 9 ? 1 : 0;
 }
