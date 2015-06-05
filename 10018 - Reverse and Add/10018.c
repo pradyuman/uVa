473efbfb6b7db0c1c5ab76833e17734ac9d1dfdd
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-06-05 14:06:55 -0500

    Working on 10018

--- 10018 - Reverse and Add/10018.c
+++ 10018 - Reverse and Add/10018.c
@@ -1,7 +1,25 @@
 /*Problem 10018*/
 #import <stdio.h>
 
+int addReverse(int);
+int palindrome(int)
+
 int main() {
+   
+   int num;
+   scanf("%d", &num);
+   
+   int added, c;
+   while (num--) {
+      c = 0;
+      scanf("%d", &added);
+      while (!palindrome(added)) {
+         added = addReverse(added);
+         c++;
+      }
+      
+      printf("%d %d\n", c. added);
+   }
    
 	return 0;
 }
