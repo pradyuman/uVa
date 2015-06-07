c940057c3b4719fa5ebcb7b07349f625e6a13766
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-06-06 19:11:29 -0500

    Working on problem 10018

--- 10018 - Reverse and Add/10018.c
+++ 10018 - Reverse and Add/10018.c
@@ -1,25 +1,46 @@
 /*Problem 10018*/
-#import <stdio.h>
+#include <stdio.h>
 
-int addReverse(int);
-int palindrome(int)
+unsigned int addReverse(unsigned int);
+int palindrome(unsigned int);
 
 int main() {
    
    int num;
    scanf("%d", &num);
    
-   int added, c;
+   unsigned int added, c;
    while (num--) {
       c = 0;
       scanf("%d", &added);
-      while (!palindrome(added)) {
+      do {
          added = addReverse(added);
          c++;
-      }
+      } while (!palindrome(added));
       
-      printf("%d %d\n", c. added);
+      printf("%d %d\n", c, added);
    }
    
 	return 0;
+}
+
+unsigned int addReverse(unsigned int added) {
+   unsigned int reverse = 0, temp = added;
+   while (temp != 0) {
+      reverse *= 10;
+      reverse += temp % 10;
+      temp /= 10;
+   }
+   
+   return added + reverse;
+}
+
+int palindrome(unsigned int added) {
+   unsigned int reverse = 0, temp = added;
+   while (temp != 0) {
+      reverse *= 10;
+      reverse += temp % 10;
+      temp /= 10;
+   }
+   return added == reverse ? 1 : 0;
 }
